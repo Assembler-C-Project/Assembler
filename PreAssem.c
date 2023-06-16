a240279a698e31996b755075cf4f6f784e9be396
vocvoc1 <vocvoc1@gmail.com>
2023-06-16 15:18:45 +0300

    Code fixed for removing macros and placing them in the correct places.

--- PreAssem.c
+++ PreAssem.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINE_LENGTH 1000
+#define MAX_LINE_LENGTH 80
 #define MAX_MACRO_COUNT 100
 #define MAX_MACRO_LINES 100
 
@@ -12,68 +12,74 @@ typedef struct {
     int lineCount;
 } Macro;
 
-Macro macroTable[MAX_MACRO_COUNT];
-int macroCount = 0;
-
-void saveMacro(FILE* file, char* macroName) {
-    char line[MAX_LINE_LENGTH];
-    int lineCount = 0;
-    macroCount++;
-
-    strncpy(macroTable[macroCount - 1].name, macroName, MAX_LINE_LENGTH);
-
-    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
-        if (strncmp(line, "endmcro", 7) == 0) {
-            break;
+void replaceTabsWithSpaces(char* str) {
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\t') {
+            str[i] = ' ';
         }
-        strncpy(macroTable[macroCount - 1].lines[lineCount], line, MAX_LINE_LENGTH);
-        lineCount++;
-        macroTable[macroCount - 1].lineCount = lineCount;
     }
 }
 
-void unfoldMacros(FILE* sourceFile, FILE* outputFile) {
+void saveMacro(FILE* file, char* macroName, Macro* macroTable[], int* macroCount) {
+    char line[MAX_LINE_LENGTH];
+    int lineCount = 0;
+    (*macroCount)++;
+
+    macroTable[*macroCount - 1] = (Macro*)malloc(sizeof(Macro));
+    strncpy(macroTable[*macroCount - 1]->name, macroName, MAX_LINE_LENGTH);
+
+    while (fgets(line, MAX_LINE_LENGTH, file) != NULL) {
+        replaceTabsWithSpaces(line);
+
+        if (strncmp(line, " endmcro", 8) == 0) {
+            break;
+        }
+        strcpy(macroTable[*macroCount - 1]->lines[lineCount], line);
+        lineCount++;
+        macroTable[*macroCount - 1]->lineCount = lineCount;
+    }
+}
+
+void unfoldMacros(FILE* sourceFile, FILE* outputFile, Macro* macroTable[], int* macroCount) {
     char line[MAX_LINE_LENGTH];
     int macroFlag = 0;
     char macroName[MAX_LINE_LENGTH];
     int i;
+    int j;
 
     while (fgets(line, MAX_LINE_LENGTH, sourceFile) != NULL) {
+        replaceTabsWithSpaces(line);
+
         if (line[0] != ' ') {
             macroFlag = 0;
         }
 
-        if (macroFlag) {
-            if (strncmp(line, "endmcro", 7) == 0) {
-                macroFlag = 0;
-            }
+        if (strncmp(line, " mcro", 5) == 0) {
+            sscanf(line, "%*s %s", macroName);
+            saveMacro(sourceFile, macroName, macroTable, macroCount);
             continue;
         }
 
         if (line[0] == ' ') {
             char macroName[MAX_LINE_LENGTH];
-            sscanf(line, "%s", macroName);
+            sscanf(line, " %s", macroName);
 
-            for (i = 0; i < macroCount; i++) {
-                if (strcmp(macroName, macroTable[i].name) == 0) {
-                    int j;
-                    for (j = 0; j < macroTable[i].lineCount; j++) {
+            for (i = 0; i < *macroCount; i++) {
+                if (strcmp(macroName, macroTable[i]->name) == 0) {
+                    macroFlag = 1;
+                    for (j = 0; j < macroTable[i]->lineCount; j++) {
                         char expandedLine[MAX_LINE_LENGTH];
-                        sprintf(expandedLine, "  %s", macroTable[i].lines[j]);
+                        sprintf(expandedLine, "%s", macroTable[i]->lines[j]);
                         fputs(expandedLine, outputFile);
                     }
-                    macroFlag = 1;
                     break;
                 }
             }
 
-            if (i == macroCount) {
+            if (i == *macroCount) {
                 fputs(line, outputFile);
             }
-        } else if (strncmp(line, "mcro", 4) == 0) {
-            macroFlag = 1;
-            sscanf(line, "%*s %s", macroName);
-            saveMacro(sourceFile, macroName);
         } else {
             fputs(line, outputFile);
         }
@@ -82,22 +88,34 @@ void unfoldMacros(FILE* sourceFile, FILE* outputFile) {
 
 
 int main() {
+    Macro* macroTable[MAX_MACRO_COUNT];
     FILE* sourceFile = fopen("source_file.txt", "r");
     FILE* outputFile = fopen("output_file.txt", "w");
+    int macroCount = 0;
+    int i;
+
+    for (i = 0; i < MAX_MACRO_COUNT; i++) {
+        macroTable[i] = NULL;
+    }
+
     if (sourceFile == NULL) {
         printf("Error opening source file.\n");
         return 1;
     }
+
     if (outputFile == NULL) {
         printf("Error opening output file.\n");
         fclose(sourceFile);
         return 1;
     }
 
-    unfoldMacros(sourceFile, outputFile);
-
+    unfoldMacros(sourceFile, outputFile, macroTable, &macroCount);
     fclose(sourceFile);
     fclose(outputFile);
+
+    for (i = 0; i < macroCount; i++) {
+        free(macroTable[i]);
+    }
 
     return 0;
 }
